704e1d4e336366ae98ffc4619e82f310cf3ea454
suhail <suhailsqm@gmail.com>
2021-06-19 23:55:27 +0530

    checkpoint5 set up models and indexfile.

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ var express = require('express')
 
 var app = express();
 
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+    
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 app.get('/movies',function(req,res){
     res.send("All Movies Data in JSON format from Mongo DB")
